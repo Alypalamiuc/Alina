6f7b4ce0e76bbb3e391f9e3e63321e64d48f126c
Alina Palamiuc <alinapalamiucgmail.com>
2019-09-07 15:17:17 +0300

    initial commit

---
+++ src/Rectangle.java
@@ -0,0 +1,25 @@
+public class Rectangle {
+       private int heigth;
+       private int length;
+      public Rectangle(int heigth,int length){
+          this.heigth = heigth;
+          this.length = length;
+
+          public int getHeigth(){
+              return heigth;
+            }
+            public void setheigth(int heigth){
+              this.heigth = heigth;
+            }
+            public int getLength(){
+              return length;
+        }
+        public void setLength(){
+              this length = length:
+          }
+          public int calculateAria(){
+              return this.heigth *this.length;
+          }
+          public static int sumArrias();
+    }
+}
---
+++ src/carsept.java
@@ -0,0 +1,11 @@
+public class carsept {
+    public static void main(String[] args) {
+        static int sum(int....elements){
+            int sum=0;
+            for (int element: elements){
+
+            }
+        }
+
+    }
+}
